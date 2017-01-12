d39afffe03091d8b62990a5e3a5af0c7b31b904f
penghongjie <penghongjie@wepiao.com>
2017-01-12 11:37:42 +0800

    add module

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+/*
+* @Author: penghongjie
+* @Date:   2017-01-11 14:32:47
+*/
+
+'use strict';
+
+const config = {
+    entry: {
+
+    },
+    output: {
+
+    },
+    modules: {
+
+    },
+    plugins: {
+
+    },
+    resolve: {
+
+    }
+}
+
+export config
